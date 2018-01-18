11db52f733f3b7e2bce79a8c4d53101b668c81f8
Seneca Sisneros <seneca.sisneros@yahoo.com>
2018-01-17 17:13:35 -0800

    added route to server for styles file

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/"));
+app.use("/styles", express.static(__dirname + "/styles"));
 
 app.listen(process.env.PORT || 8080);
